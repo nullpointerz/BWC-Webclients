d0111156b53a0dec441002080d12128bf6e5ab71
Bergendahl94 <william_bergendahl@hotmail.com>
2014-05-27 16:04:55 +0200

    Added a command log displayer and sonar and temp features

--- BWC/Index.php
+++ BWC/Index.php
@@ -20,19 +20,19 @@ if (!isset($_SESSION["User"])) {
     <p> A = Turn Left</p>
     <p> S = Backward</p>
     <p> D = Turn Right </p>
-    <hr> </hr>
     <p><h4><li>Extra Features </li></h4></p>
-    <p> F = Turn Off lights</p>
-    <p> G = Save GPS location</p>
-    <p> H = Honk</p>
-    <p> Enter = Shoot</p>
     <p> Space = Emergency Brake</p>
+    <p> T = Get Temperature</p>
+    <hr> </hr>
+    
+    <p><h3>Command Status</h3></p>
+    <p id="commands">Server commands</p>
     </div>
     </div>
     <div id="mainpart" style="overflow:auto;"> 
     <div id="cam-stream">
    
-   <img src="http://192.168.0.16:8080/?action=stream" alt="No Connection To The Pi Could be Established!" name="camimage" width="580" height="440" border="0" class="bild-border">
+   <img src="http://192.168.2.5:8080/?action=stream" alt="No Connection To The Pi Stream Could be Established!" name="camimage" width="580" height="440" border="0" class="bild-border">
   <p id="status">Keyboard Command Status</p>
     </div>
          <div id="mp-text"> 
@@ -53,15 +53,17 @@ function WebSocketTest()
   if ("WebSocket" in window)
   {
 	  console.log('Websocket Open Logged');
+	  commands.innerHTML = "Trying to establish websocket connection..";
     
      // Let us open a web socket
-     var ws = new WebSocket("ws://192.168.2.5:50007/");
+     var ws = new WebSocket("ws://192.168.2.4:50007");
      ws.onopen = function()
      {
-		
+		commands.innerHTML = "Websocket connection established!";
         // Web Socket is connected, send data using send()
         ws.send(Message);
 		console.log('Message Sent Logged: ' + Message); 
+        commands.innerHTML = "Message sent: " + Message;
 		 
      };
      ws.onmessage = function (evt) 
@@ -69,16 +71,20 @@ function WebSocketTest()
 	 
         var received_msg = evt.data;
 		console.log('Message Recieved Back Logged: ' + received_msg);
+		commands.innerHTML = "Message from server: " + Message;
 	 
      };
      ws.onclose = function()
      { 
         // websocket is closed.
         console.log('Closing socket Logged');
+		commands.innerHTML = "Closing the socket";
      };
 	 ws.onerror = function(evt) 
 	 {
 		 console.log('Websocket Error' + evt.data);
+		 commands.innerHTML = "Error: " + evt.data;
+		 
 	 }
   }
   else
@@ -94,11 +100,13 @@ document.onkeydown = function(event) {
 	var Left = 65;
 	var Right = 68;
 	var Emergency_Stop = 32;
+	var Temperature = 84;
+	var q = 81;
 	
 	var key_press = String.fromCharCode(event.keyCode);
 	var key_code = event.keyCode;
 	var status = document.getElementById('status');
-	status.innerHTML = "DOWN Event Fired For : "+key_press;
+	status.innerHTML = "Command Executed!";
 	if (key_code == Forward && once == true) {
 		
 		status.innerHTML = "Moving Forward"
@@ -129,6 +137,20 @@ document.onkeydown = function(event) {
 		WebSocketTest();
 		once = false;
 	}
+	else if (key_code == Temperature && once == true) {
+		status.innerHTML = "Getting Temperature"
+		Message = "GetTemp";
+		WebSocketTest();
+		once = false;
+	}
+	//This else statement is only supposed to be a function for the simulator
+	else if (key_code == q) {
+		status.innerHTML = "Sonar"
+		Message = "GetSonar";
+		WebSocketTest();
+	
+	}
+	
 	
 }
 var once = true;
@@ -163,5 +185,11 @@ document.onkeyup = function(event){
 		once = true;
 		
 	}	
+	else if (event.keyCode == 84 && once == false) {
+		status.innerHTML = "stopping temp";
+		Message = "stop";
+		once = true;
+		
+	}
 }
 </script>
